7e9bc822f71a32e1fbeb058113d9c9ffc9017234
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-09-29 16:13:11 +0300

    fix tailwindcss

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+type HeaderProps = {};
+
+const Header: FC<HeaderProps> = (props) => {
+  return <header className="m-10">I am Header</header>;
+};
+
+export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,41 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    plugins: [],
+    opacity: {
+      "0": "0",
+      "20": "0.2",
+      "40": "0.4",
+      "60": "0.6",
+      "80": "0.8",
+      "100": "1",
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      blue: "#1fb6ff",
+      purple: "#7e5bef",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
   },
-  plugins: [],
-}
-export default config
+};
+export default config;
