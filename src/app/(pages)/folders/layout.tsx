bf06e511f223168a82ae669946f98124746b8c52
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-10-06 18:21:35 +0300

    Update layout.tsx

--- src/app/(pages)/folders/layout.tsx
+++ src/app/(pages)/folders/layout.tsx
@@ -1,12 +1,19 @@
-
+import { addFolders } from "@/api/controllers/folder/addFolder";
+import { getFolders } from "@/api/controllers/folder/getFolders";
+import { getUsers } from "@/api/controllers/user/getUsers";
 import FoldersNav from "@/components/foldersNav/foldersNav";
 import Title from "@/components/title/title";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const folders = await getFolders();
   return (
     <>
       <Title title="Папки" />
-      <FoldersNav />
+      <FoldersNav folders={folders} />
       {children}
     </>
   );
