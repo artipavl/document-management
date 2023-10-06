5c1a32ad411c4c4bc078214354dd2af82eda3c86
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-10-06 18:22:45 +0300

    Update foldersNav.tsx

--- src/components/foldersNav/foldersNav.tsx
+++ src/components/foldersNav/foldersNav.tsx
@@ -1,40 +1,25 @@
-import { getFolders } from "@/api/controllers/folder/getFolders";
 import Link from "next/link";
 import React, { FC } from "react";
 
 import styles from "./foldersNav.module.scss";
-import { addFolders } from "@/api/controllers/folder/addFolder";
-import { getUsers } from "@/api/controllers/user/getUsers";
+import { AddForm } from "../addFolderForm/addFolderForm";
 
-type FoldersNavProps = {};
+type FoldersNavProps = {
+  folders: IFolder[];
+};
 
-const FoldersNav: FC<FoldersNavProps> = async (props) => {
-  const folders = await getFolders();
-  const user = await getUsers();
+const FoldersNav: FC<FoldersNavProps> = ({ folders }) => {
   return (
     <ul className={styles.list}>
       {folders.map((folder) => (
-        <li key={folder.id}>
-          <Link href={folder.href} className={styles.item}>
+        <li key={folder._id}>
+          <Link href={"/folders/" + folder.href} className={styles.item}>
             {folder.name.toUpperCase()}
           </Link>
         </li>
       ))}
       <li>
-        <form
-        //   onSubmit={(e) => {
-        //     e.preventDefault();
-        //     const name = e.currentTarget.children[0] as HTMLInputElement;
-        //     if (name.value) {
-        //       addFolders({ name: name.value }).finally(() =>
-        //         e.currentTarget.remove()
-        //       );
-        //     }
-        //   }}
-        >
-          <input type="text" name="name" />
-        </form>{" "}
-        <button type="submit">+</button>
+        <AddForm />
       </li>
     </ul>
   );
