556d56942fe7e236170fed98a653a72d642fcc17
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-10-06 18:21:44 +0300

    Update page.tsx

--- src/app/(pages)/folders/page.tsx
+++ src/app/(pages)/folders/page.tsx
@@ -1,14 +1,12 @@
+import { getUsers } from "@/api/controllers/user/getUsers";
 import React, { FC } from "react";
 
 type PageProps = {};
 
-const Folders: FC<PageProps> = (props) => {
-  return (
-    <div>
-      пуста
-      
-    </div>
-  );
+const Folders: FC<PageProps> = async (props) => {
+  // const user = await getUsers();
+  // console.log(user);
+  return <div>пуста</div>;
 };
 
 export default Folders;
