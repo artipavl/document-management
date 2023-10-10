47d909a83ca55ad15679c11c5fae21dc57dc41ac
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-10-10 14:14:48 +0300

    add controllers Addressee

---
+++ src/api/controllers/addressee/addAddressee.ts
@@ -0,0 +1,23 @@
+import AddresseeModel from "@/api/models/addressee";
+import connectDB from "../../connect-db";
+import FolderModel from "@/api/models/folder";
+
+export const addAddressee = async ({
+  name,
+  email,
+  phone = "-",
+  underAddressee = [],
+}: IAddAddressee) => {
+  try {
+    await connectDB();
+    await AddresseeModel.create({
+      name,
+      email,
+      phone,
+      underAddressee,
+    });
+    // return Addressee;
+  } catch (error) {
+    console.log(error);
+  }
+};
