f54d706748a3608395a43d60ed248b09e8808aa4
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-10-19 22:29:38 +0300

    update form

--- src/components/addAddresseeForm/actions.ts
+++ src/components/addAddresseeForm/actions.ts
@@ -1,11 +1,17 @@
 "use server";
 
 import { addAddressee } from "@/api/controllers/addressee/addAddressee";
+import { updateAddresseeById } from "@/api/controllers/addressee/updateAddresseeById";
 import { revalidatePath } from "next/cache";
 
-export async function createAddressee(values: IAddAddressee) {
+export async function createAddressee(values: IAddAddressee, id?: string) {
   try {
-    await addAddressee(values);
+    console.log(id);
+    if (id) {
+      await updateAddresseeById(values, id);
+    } else {
+      await addAddressee(values);
+    }
     return revalidatePath("/");
   } catch (e) {
     return { message: "Failed to create" };
