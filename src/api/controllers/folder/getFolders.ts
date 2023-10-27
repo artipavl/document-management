1a0cf0de189d2f98eb9bc45c4e1ed1465682e2cd
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-10-27 19:37:03 +0300

    Update getFolders.ts

--- src/api/controllers/folder/getFolders.ts
+++ src/api/controllers/folder/getFolders.ts
@@ -4,8 +4,15 @@ import FolderModel from "@/api/models/folder";
 export const getFolders = async (): Promise<IFolder[]> => {
   try {
     await connectDB();
-    const Folders = await FolderModel.find({});
-    return Folders as IFolder[];
+    const folders = await FolderModel.find({});
+    const folderAsString: IFolder[] = folders.map((folder) => {
+      return {
+        ...folder.toJSON(),
+        _id: folder._id.toString(),
+      };
+    });
+
+    return folderAsString;
   } catch (error) {
     console.log(error);
     return [];
