c760b7a00595044a5162ba6438c2ba466955fc63
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-10-28 19:08:13 +0300

    Update user.ts

--- src/api/models/user.ts
+++ src/api/models/user.ts
@@ -1,14 +1,39 @@
-import { prop, getModelForClass } from "@typegoose/typegoose";
+import { prop, getModelForClass, type Ref } from "@typegoose/typegoose";
+import { Department } from "./department";
 
 class User {
   @prop({ required: true })
   public name!: string;
 
+  @prop({ required: true })
+  public surname!: string;
+
+  @prop()
+  public lastName?: string;
+
+  @prop({ required: true })
+  public age!: number;
+
   @prop({ required: true, unique: true })
   public email!: string;
 
+  @prop({ required: false, default: "" })
+  public phone?: string;
+
   @prop({ required: true })
   public password!: string;
+
+  @prop()
+  public verificationCode?: string;
+
+  @prop()
+  public token?: string;
+
+  @prop()
+  public jobPosition?: string;
+
+  @prop({ ref: () => Department })
+  public department?: Ref<Department>;
 }
 
 const UserModel = getModelForClass(User);
