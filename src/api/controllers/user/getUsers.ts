40a1ed5d6b11c7b307d897c6c15d4ee3cd0c681c
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-10-28 19:07:57 +0300

    Update getUsers.ts

--- src/api/controllers/user/getUsers.ts
+++ src/api/controllers/user/getUsers.ts
@@ -4,8 +4,22 @@ import UserModel from "../../models/user";
 export const getUsers = async () => {
   try {
     await connectDB();
-    const User = await UserModel.find({});
-    return User;
+    const users = await UserModel.find(
+      {},
+      "name surname age email phone departmen"
+    );
+
+    const usersAsString: IUser[] = users.map((user) => {
+      return {
+        ...user.toJSON(),
+        _id: user._id.toString(),
+        ...(user.department
+          ? { department: user.department.toString() }
+          : { department: undefined }),
+      };
+    });
+
+    return usersAsString;
   } catch (error) {
     console.log(error);
   }
