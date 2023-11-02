36d1c21a693c29137f12b06cf00ffdda1f572266
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-11-02 14:15:47 +0200

    Create document.ts

---
+++ src/api/models/document.ts
@@ -0,0 +1,48 @@
+import { prop, getModelForClass, type Ref } from "@typegoose/typegoose";
+import { Addressee } from "./addressee";
+import { Folder } from "./folder";
+
+export class Document {
+  @prop({ required: true })
+  public number!: string;
+
+  @prop({ required: true })
+  public date!: string;
+
+  @prop({ required: false, default: "" })
+  public description?: string;
+
+  @prop({ ref: Addressee })
+  public addressee?: Ref<Addressee>;
+
+  @prop({ ref: Addressee.name })
+  public addresseeSignature?: Ref<string>;
+
+  @prop({ required: true })
+  public title!: string;
+
+  @prop()
+  public documentDate?: string;
+
+  @prop()
+  public documentnNmber?: string;
+
+  @prop()
+  public documentnType?: string;
+
+  @prop()
+  public remark!: string;
+
+  @prop({ required: true, default: false })
+  public control!: boolean;
+
+  @prop()
+  public controlFrequency?: string;
+
+  @prop({ ref: Folder })
+  public folder!: Ref<Folder>;
+}
+
+const DocumentModel = getModelForClass(Document);
+
+export default DocumentModel;
