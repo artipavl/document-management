94968d30630206dbad23a5c1b20af79a389beeae
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-11-22 17:59:47 +0200

    Update addFolder.ts

--- src/api/controllers/folder/addFolder.ts
+++ src/api/controllers/folder/addFolder.ts
@@ -1,3 +1,4 @@
+import translit from "@/helpers/translit";
 import connectDB from "../../connect-db";
 import FolderModel from "@/api/models/folder";
 
@@ -14,58 +15,4 @@ export const addFolders = async ({ name }: IAddFolder) => {
   }
 };
 
-function translit(str: string) {
-  const space = "-";
-  let link = "";
-  const transl = {
-    а: "a",
-    б: "b",
-    в: "v",
-    г: "g",
-    д: "d",
-    е: "e",
-    є: "ie",
-    ж: "zh",
-    з: "z",
-    и: "y",
-    і: "i",
-    й: "i",
-    ї: "j",
-    к: "k",
-    до: "k",
-    л: "l",
-    м: "m",
-    н: "n",
-    о: "o",
-    про: "o",
-    п: "p",
-    р: "r",
-    с: "s",
-    т: "t",
-    у: "u",
-    ф: "f",
-    х: "h",
-    ц: "c",
-    ч: "ch",
-    ш: "sh",
-    щ: "sh",
-    ъ: space,
-    ь: space,
-    ю: "yu",
-    я: "ya",
-  };
-  if (str != "") str = str.toLowerCase();
-  for (let i = 0; i < str.length; i++) {
-    const char = str.charAt(i);
-    if (/[а-яе]/.test(char)) {
-      // замінюємо символи російською
-      link += transl[char as keyof typeof transl];
-    } else if (/[a-z0-9]/.test(char)) {
-      // символи на анг. залишаємо як є
-      link += char;
-    } else {
-      if (link.slice(-1) !== space) link += space; // інші символи замінюємо на space
-    }
-  }
-  return link;
-}
+
