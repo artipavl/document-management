0107779d4d207e92ff64b9a090f0e84ac099760e
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-11-24 17:42:35 +0200

    Update globals.d.ts

--- globals.d.ts
+++ globals.d.ts
@@ -24,33 +24,23 @@ type PipelineStage =
   | { $project: object }
   | { $match: object };
 
-interface IFolder {
-  _id: string;
-  name: string;
-  href: string;
-}
-
-interface IPeriodicity {
-  _id: string;
-  name: string;
-  href: string;
-}
-
-interface IImportance {
-  _id: string;
-  name: string;
-  href: string;
-}
-
-interface IExecution {
-  _id: string;
-  name: string;
-  href: string;
-}
-
 interface IAddSelect {
   name: string;
 }
+
+interface ISelect {
+  _id: string;
+  name: string;
+  href: string;
+}
+
+type IFolder = ISelect;
+
+type IPeriodicity = ISelect;
+
+type IImportance = ISelect;
+
+type IExecution = ISelect;
 
 interface IAddUnderAddressee {
   name: string;
