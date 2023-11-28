ba809d8c45e0478de7dc5d714938b740f035190e
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-11-28 17:59:32 +0200

    add DepartmentsStructure

--- src/app/(pages)/structure/page.tsx
+++ src/app/(pages)/structure/page.tsx
@@ -1,12 +1,17 @@
+import { getDepartmentAll } from "@/api/controllers/department/getDepartmentAll";
+import { getDepartments } from "@/api/controllers/department/getDepartments";
+import DepartmentsStructure from "@/components/departmentsStructure/departmentsStructure";
 import Title from "@/components/title/title";
 import React, { FC } from "react";
 
 type PageProps = {};
 
-const Structure: FC<PageProps> = (props) => {
+const Structure: FC<PageProps> = async (props) => {
+  const { departments } = await getDepartmentAll();
   return (
     <div>
       <Title title="Структура" />
+      <DepartmentsStructure departments={departments} />
     </div>
   );
 };
